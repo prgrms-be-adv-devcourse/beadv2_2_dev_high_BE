fef62fa9f83a88d548ca3383113900e7b6eb6c65
jtlee <jongtan.lee@gmail.com>
2025-12-04 11:14:19 +0900

    init
    init: .gitignore add .env**
    
    init : 공통모듈에 유틸,응답dto 추가
    
    - build.gradle 추가
    
    init settings.gradle
    
    init properties를 yml로 변경
    
    init : common 모듈화
    
    - bootjar false
    
    init : eureka ,discovery, config 등
    
    init root build.gradle-Swagger 추가
    
    init : 서버 포트 수정 ,gateway route추가
    
    init : 필요없는 파일 제거

--- alarm/src/main/java/com/dev_high/notice/NoticeApplication.java
+++ alarm/src/main/java/com/dev_high/notice/NoticeApplication.java
@@ -1,4 +1,4 @@
-package com.dev_high.auth;
+package com.dev_high.notice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class AuthApplication {
+public class NoticeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthApplication.class, args);
+        SpringApplication.run(NoticeApplication.class, args);
     }
 
 }
--- auction/src/main/java/com/dev_high/auction/AuctionApplication.java
+++ auction/src/main/java/com/dev_high/auction/AuctionApplication.java
@@ -2,8 +2,10 @@ package com.dev_high.auction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuctionApplication {
 
     public static void main(String[] args) {
