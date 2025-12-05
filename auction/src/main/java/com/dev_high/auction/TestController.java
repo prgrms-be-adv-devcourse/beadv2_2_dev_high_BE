d0776172f61fea0c039947550a3e8a9d5299d4f9
Geun-Hwan <rmsghks1026@gmail.com>
2025-12-05 15:30:31 +0900

    feat: querydsl 설정 및 자잘한추가
    - CustomException 추가
    - ApiResponseDto 수정
    
    Close #12

--- auction/src/main/java/com/dev_high/auction/TestController.java
+++ auction/src/main/java/com/dev_high/auction/TestController.java
@@ -1,7 +1,13 @@
 package com.dev_high.auction;
 
 import com.dev_high.auction.domain.Auction;
+import com.dev_high.auction.domain.QAuction;
+import com.dev_high.common.exception.CustomException;
+import com.querydsl.core.QueryFactory;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+import javax.naming.AuthenticationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,12 +21,18 @@ public class TestController {
 
   private  final TestRepository  testRepository;
 
+  private  final JPAQueryFactory queryFactory;
+
   @GetMapping
   public Auction test() {
-
+    QAuction qAuction = new QAuction("a1");
     Auction auction =new Auction("TEST",100L, LocalDateTime.now(),LocalDateTime.now(),"TEST");
+    testRepository.saveAndFlush(auction);
 
-    return testRepository.save(auction);
+//    return queryFactory.select(qAuction).from(qAuction).fetchOne();
+
+
+
   }
 
 
