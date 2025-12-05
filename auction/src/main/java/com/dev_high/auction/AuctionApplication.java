d745e565133e3bd4590089b496d79e738e2f8cbe
Geun-Hwan <rmsghks1026@gmail.com>
2025-12-05 15:30:31 +0900

    feat: querydsl 설정 및 자잘한추가
    - CustomException 추가
    - ApiResponseDto 수정
    
    Close #12

--- auction/src/main/java/com/dev_high/auction/AuctionApplication.java
+++ auction/src/main/java/com/dev_high/auction/AuctionApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+/* common에 있는 config 스캔*/
+@SpringBootApplication(scanBasePackages = {"com.dev_high.auction", "com.dev_high.common"})
 @EnableDiscoveryClient
 public class AuctionApplication {
 
