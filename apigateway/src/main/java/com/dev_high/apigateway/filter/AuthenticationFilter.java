61a29046e17e3dab594f94fdf879b11d6b736215
Geun-Hwan <rmsghks1026@gmail.com>
2025-12-09 19:37:49 +0900

    fix: 인증 로직 수정
    Close #37

--- apigateway/src/main/java/com/dev_high/apigateway/filter/AuthenticationFilter.java
+++ apigateway/src/main/java/com/dev_high/apigateway/filter/AuthenticationFilter.java
@@ -1,36 +1,39 @@
 package com.dev_high.apigateway.filter;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Claims;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
-public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+public class AuthenticationFilter implements GlobalFilter, Ordered {
 
-    public AuthenticationFilter() {
-        super(Config.class);
+  @Override
+  public int getOrder() {
+    return -1; // 인증 이후, 라우팅 전에
+  }
+
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    Object attribute = exchange.getAttribute("User");
+
+    if (attribute instanceof Claims claims) {
+      String userId = claims.getSubject();
+
+      ServerHttpRequest mutatedRequest = exchange.getRequest()
+          .mutate()
+          .header("X-User-Id", userId)
+          .build();
+
+      return chain.filter(exchange.mutate().request(mutatedRequest).build());
     }
 
-    @Override
-    public GatewayFilter apply(AuthenticationFilter.Config config) {
-        return ((exchange, chain) -> {
-            Object attribute = exchange.getAttribute("User");
-            if(attribute instanceof Claims claims) {
-                String userId = claims.getSubject();
-                ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
-                        .header("X-User-Id", userId)
-                        .build();
-                return chain.filter(exchange.mutate().request(mutatedRequest).build());
-            }
-            return chain.filter(exchange);
-        });
-    }
-
-    public static class Config {
-
-    }
+    return chain.filter(exchange);
+  }
 }
