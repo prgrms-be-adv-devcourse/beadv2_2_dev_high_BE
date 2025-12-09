ca1c7aadc7ade6e23e440920cb31af61725889ef
Geun-Hwan <rmsghks1026@gmail.com>
2025-12-09 19:37:49 +0900

    fix: 인증 로직 수정
    Close #37

---
+++ common/src/main/java/com/dev_high/common/config/WebConfig.java
@@ -0,0 +1,35 @@
+package com.dev_high.common.config;
+
+import com.dev_high.common.interceptor.UserContextInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  private final UserContextInterceptor userContextInterceptor;
+
+  public WebConfig(UserContextInterceptor userContextInterceptor) {
+    this.userContextInterceptor = userContextInterceptor;
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+
+    registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("*")
+        .allowedHeaders("*")
+        .allowCredentials(false)
+        .maxAge(3600);
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    // 모든 경로에 적용
+    registry.addInterceptor(userContextInterceptor)
+        .addPathPatterns("/**");
+  }
+}
