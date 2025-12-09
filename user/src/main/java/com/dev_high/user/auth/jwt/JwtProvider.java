4bbbad1cc8508e04357396be527bd00535603828
quokka <qkforest@gmail.com>
2025-12-09 15:28:49 +0900

    refactor: ë©apigateway 수정

--- user/src/main/java/com/dev_high/user/auth/jwt/JwtProvider.java
+++ user/src/main/java/com/dev_high/user/auth/jwt/JwtProvider.java
@@ -1,5 +1,6 @@
 package com.dev_high.user.auth.jwt;
 
+import io.jsonwebtoken.Claims;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Value;
 import io.jsonwebtoken.Jwts;
@@ -10,16 +11,23 @@ import java.util.Date;
 public class JwtProvider {
     private final String secretKey;
     private final long accessTokenExpiration;
+    private final long refreshTokenExpiration;
 
     public JwtProvider(@Value("${jwt.secret}") String secretKey,
-                       @Value("${jwt.access-token-expiration}") long accessTokenExpiration) {
+                       @Value("${jwt.access-token-expiration}") long accessTokenExpiration,
+                      @Value("${jwt.refresh-token-expiration}") long refreshTokenExpiration) {
 
         this.secretKey = secretKey;
         this.accessTokenExpiration = accessTokenExpiration;
+        this.refreshTokenExpiration = refreshTokenExpiration;
     }
 
     public String generateAccessToken(String userId, String role){
         return generateToken(userId, role, accessTokenExpiration);
+    }
+
+    public String generateRefreshToken(String userId, String role){
+        return generateToken(userId, role, refreshTokenExpiration);
     }
 
     public String generateToken(String userId,
@@ -34,6 +42,14 @@ public class JwtProvider {
                 .setExpiration(expireDate)
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
+    }
+
+    public Claims parseToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
 }
--- user/src/main/java/com/dev_high/user/auth/presentation/AuthController.java
+++ user/src/main/java/com/dev_high/user/auth/presentation/AuthController.java
@@ -5,7 +5,10 @@ import com.dev_high.user.auth.application.AuthService;
 import com.dev_high.user.auth.application.dto.LoginCommand;
 import com.dev_high.user.auth.application.dto.LoginInfo;
 import com.dev_high.common.dto.ApiResponseDto;
+import com.dev_high.user.auth.application.dto.TokenCommand;
+import com.dev_high.user.auth.application.dto.TokenInfo;
 import com.dev_high.user.auth.presentation.dto.LoginRequest;
+import com.dev_high.user.auth.presentation.dto.TokenRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +29,14 @@ public class AuthController {
                 request.password()
         );
         return authService.login(command);
+    }
+
+    @PostMapping("/refresh/token")
+    public ApiResponseDto<TokenInfo> refreshToken(@RequestBody TokenRequest request){
+        TokenCommand command = new TokenCommand(
+                request.refreshToken()
+        );
+        return authService.refreshToken(command);
     }
 
 }
