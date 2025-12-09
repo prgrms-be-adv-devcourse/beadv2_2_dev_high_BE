976f02fa9e4e29e9653d3a928706804d2781fa72
jtlee <jongtan.lee@gmail.com>
2025-12-09 12:32:54 +0900

    feat: 예치금 계좌 생성 수정 Request DTO 추가 및 API parameter 수정

--- deposit/src/main/java/com/dev_high/deposit/presentation/DepositController.java
+++ deposit/src/main/java/com/dev_high/deposit/presentation/DepositController.java
@@ -2,6 +2,7 @@ package com.dev_high.deposit.presentation;
 
 import com.dev_high.deposit.application.DepositService;
 import com.dev_high.deposit.application.dto.DepositInfo;
+import com.dev_high.deposit.presentation.dto.DepositCreateRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,10 +16,10 @@ public class DepositController {
     private final DepositService depositService;
 
     @Operation(summary = "예치금 계좌 생성", description = "예치금 계좌를 생성하고 저장")
-    @PostMapping("/{userId}")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public DepositInfo createDepositAccount(
-            @PathVariable @Validated String userId) {
-        return depositService.createDepositAccount(userId);
+            @RequestBody @Validated DepositCreateRequest request) {
+        return depositService.createDepositAccount(request.toCommand());
     }
 }
