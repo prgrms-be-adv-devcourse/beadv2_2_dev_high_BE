723c02e60e75ef60549a2c921d39360eb06f46f7
Geun-Hwan <rmsghks1026@gmail.com>
2025-12-13 00:56:29 +0900

    feat: 주문 생성 리스너 추가 기존 batch 수정
    - 주문 생성 이벤트 구독 후 처리 (완료 후 알림이벤트 발행)
    - 기존 경매 상태 job 하나로 통합(상태 업데이트,배송,취소 확정 등)

---
+++ order/src/main/java/com/dev_high/order/batch/OrderStatusScheduler.java
@@ -0,0 +1,38 @@
+package com.dev_high.order.batch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OrderStatusScheduler {
+
+
+  private final JobLauncher jobLauncher;
+  private final Job orderStatusJob;
+
+  /**
+   * 매일 새벽 n시 n분에 실행
+   */
+  @Scheduled(cron = "0 11 1 * * *")
+  public void runOrderStatusJob() {
+    try {
+      JobParameters params = new JobParametersBuilder()
+          .addLong("time", System.currentTimeMillis()) // 매 실행마다 다른 파라미터
+          .toJobParameters();
+
+      jobLauncher.run(orderStatusJob, params);
+      log.info("OrderStatusJob 실행 완료");
+    } catch (Exception e) {
+      log.error("OrderStatusJob 실행 중 오류", e);
+    }
+  }
+}
