715130a56c77cb924ee3f117614253e445d4d89e
jtlee <jongtan.lee@gmail.com>
2025-12-12 16:46:53 +0900

    feat: 누락부분 추가

--- notification/src/main/java/com/dev_high/notification/NotificationApplication.java
+++ notification/src/main/java/com/dev_high/notification/NotificationApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(scanBasePackages = {"com.dev_high.common"})
+@SpringBootApplication(scanBasePackages = {"com.dev_high.notification", "com.dev_high.common"})
 @EnableDiscoveryClient
 public class NotificationApplication {
 
