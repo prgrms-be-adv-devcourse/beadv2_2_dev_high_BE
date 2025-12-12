ab18e4f9fdbd7a676bb402014cfa1f23fcc899cc
Geun-Hwan <rmsghks1026@gmail.com>
2025-12-13 05:58:43 +0900

    chore: 어노테이션 위치 변경

--- auction/src/main/java/com/dev_high/auction/batch/BatchConfig.java
+++ auction/src/main/java/com/dev_high/auction/batch/BatchConfig.java
@@ -8,12 +8,10 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableScheduling
 public class BatchConfig {
 
   private final JobRepository jobRepository;
