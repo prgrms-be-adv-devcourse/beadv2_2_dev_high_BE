432fcbdaf25f1a24e6f9c1d3b129ef319f4f7048
Geun-Hwan <rmsghks1026@gmail.com>
2025-12-15 11:06:03 +0900

    fix: 경매 수정
    - Deprecated 토픽 제거

--- auction/src/main/java/com/dev_high/auction/application/AuctionEventListener.java
+++ auction/src/main/java/com/dev_high/auction/application/AuctionEventListener.java
@@ -1,12 +1,16 @@
 package com.dev_high.auction.application;
 
-import com.dev_high.auction.infrastructure.bid.AuctionParticipationJpaRepository;
+import com.dev_high.common.kafka.KafkaEventEnvelope;
 import com.dev_high.common.kafka.KafkaEventPublisher;
 import com.dev_high.common.kafka.event.auction.AuctionCreateSearchRequestEvent;
+import com.dev_high.common.kafka.event.auction.AuctionDepositRefundRequestEvent;
 import com.dev_high.common.kafka.event.auction.AuctionUpdateSearchRequestEvent;
 import com.dev_high.common.kafka.topics.KafkaTopics;
+import com.dev_high.common.util.JsonUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -18,20 +22,18 @@ public class AuctionEventListener {
 
   private final KafkaEventPublisher eventPublisher;
 
-
-  private final AuctionService auctionService;
-  private final BidService bidService;
-  private final AuctionParticipationJpaRepository auctionParticipationJpaRepository;
-//
+  private final BidRecordService recordService;
 
   // TEST
-// @KafkaListener(topics = KafkaTopics.AUCTION_NOTIFICATION_REQUESTED)
-//  public void test(KafkaEventEnvelope<?> envelope, ConsumerRecord<?, ?> record) {
-  // 리스너 및 로깅 테스트
-//    Object val = envelope.payload();
+  @KafkaListener(topics = KafkaTopics.DEPOSIT_AUCTION_REFUND_RESPONSE)
+  public void test(KafkaEventEnvelope<?> envelope, ConsumerRecord<?, ?> record) {
 
-  // 서비스로직
-//  }
+    AuctionDepositRefundRequestEvent val = JsonUtil.fromPayload(envelope.payload(),
+        AuctionDepositRefundRequestEvent.class);
+
+    recordService.markDepositRefunded(val.auctionId(), val.userIds());
+
+  }
 
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   public void handle(AuctionCreateSearchRequestEvent event) {
