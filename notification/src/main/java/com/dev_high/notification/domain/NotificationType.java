09ce5bf86b203c2c40996b7a89f073244e45c83d
jtlee <jongtan.lee@gmail.com>
2025-12-16 17:05:30 +0900

    fix: 알림 서비스 카프카 이벤트 리스너 추가

--- notification/src/main/java/com/dev_high/notification/domain/NotificationType.java
+++ notification/src/main/java/com/dev_high/notification/domain/NotificationType.java
@@ -2,12 +2,22 @@ package com.dev_high.notification.domain;
 
 /*
 * 알림 타입 정의
-* WISH : 위시리스트
 * AUCTION : 경매
+* DEPOSIT : 예치금
 * ORDER : 주문
+* PRODUCT : 상품
+* SEARCH : 검색
+* SETTLEMENT : 정산
+* USER : 사용자
+* GENERAL : 일반
 * */
 public enum NotificationType {
-    WISH,
     AUCTION,
-    ORDER
+    DEPOSIT,
+    ORDER,
+    PRODUCT,
+    SEARCH,
+    SETTLEMENT,
+    USER,
+    GENERAL
 }
