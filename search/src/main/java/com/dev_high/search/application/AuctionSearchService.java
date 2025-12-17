9421cc4db5942ddb8c684c4c755ecebe3d9fe9e6
quokka <qkforest@gmail.com>
2025-12-17 11:35:38 +0900

    feat: document에서 redirectUrl 삭제 및 경매 시작 일자 및 시작 가격 범위 필터 추가

--- search/src/main/java/com/dev_high/search/application/AuctionSearchService.java
+++ search/src/main/java/com/dev_high/search/application/AuctionSearchService.java
@@ -3,6 +3,7 @@ package com.dev_high.search.application;
 
 import co.elastic.clients.elasticsearch._types.FieldValue;
 import co.elastic.clients.elasticsearch._types.SortOrder;
+import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.Operator;
 import com.dev_high.common.dto.ApiResponseDto;
 import com.dev_high.common.kafka.event.auction.*;
@@ -12,10 +13,12 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.elasticsearch.client.elc.NativeQuery;
+import org.springframework.data.elasticsearch.client.elc.*;
 import org.springframework.data.elasticsearch.core.*;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -40,7 +43,6 @@ public class AuctionSearchService {
                 .auctionStartAt(request.auctionStartAt())
                 .auctionEndAt(request.auctionEndAt())
                 .sellerId(request.sellerId())
-                .redirectUrl("/" + request.productId())
                 .build();
         auctionSearchRepository.save(document);
     }
@@ -60,56 +62,86 @@ public class AuctionSearchService {
         auctionSearchRepository.save(document);
     }
 
-    public ApiResponseDto<Page<AuctionDocument>> searchAuctions(String keyword, List<String> categories, String status, Pageable pageable) {
+    public ApiResponseDto<Page<AuctionDocument>> searchAuctions(String keyword,
+                                                                List<String> categories,
+                                                                String status,
+                                                                BigDecimal minStartPrice,
+                                                                BigDecimal maxStartPrice,
+                                                                LocalDateTime startFrom,
+                                                                LocalDateTime startTo,
+                                                                Pageable pageable) {
+        BoolQuery.Builder boolQuery = new BoolQuery.Builder();
+        // 키워드 검색
+        if (keyword != null && !keyword.isBlank()) {
+            // 순서 그대로 or 단어 사이에 최대 한 단어 허용
+            boolQuery.should(m -> m.matchPhrase(mm -> mm
+                    .field("productName")
+                    .query(keyword)
+                    .slop(1)
+                    .boost(2.0f)
+            ));
+            //단어 중 하나만 있어도 매칭
+            boolQuery.should(m -> m.match(mm -> mm
+                    .field("productName")
+                    .query(keyword)
+                    .operator(Operator.Or)
+                    .boost(1.0f)
+            ));
+            boolQuery.should(m -> m.match(mm -> mm
+                    .field("description")
+                    .query(keyword)
+                    .operator(Operator.Or)
+                    .boost(1.0f)
+            ));
+            boolQuery.minimumShouldMatch("1");
+        }
+        // 카테고리 필터
+        if (categories != null && !categories.isEmpty()) {
+            boolQuery.filter(f -> f.terms(t -> t
+                    .field("categories")
+                    .terms(tv -> tv.value(
+                            categories.stream()
+                                    .map(FieldValue::of)
+                                    .toList()
+                    ))
+            ));
+        }
+
+        // 상태 필터
+        if (status != null && !status.isBlank()) {
+            boolQuery.filter(f -> f.term(t -> t.field("status").value(status)));
+        }
+
+        // 경매 시작일
+        if(minStartPrice != null || maxStartPrice != null) {
+            boolQuery.filter(f -> f.range(r -> r.number(n -> {
+                n.field("startPrice");
+                if (minStartPrice != null) {
+                    n.gte(minStartPrice.doubleValue());
+                }
+                if (maxStartPrice != null) {
+                    n.lte(maxStartPrice.doubleValue());
+                }
+                return n;
+            })));
+        }
+
+        // 경매 시작일
+        if(startFrom != null || startTo != null) {
+            boolQuery.must(q -> q.range(r -> r.date(d -> {
+                d.field("auctionStartAt");
+                if (startFrom != null) {
+                    d.gte(startFrom.toString());
+                }
+                if (startTo != null) {
+                    d.lte(startTo.toString());
+                }
+                return d;
+            })));
+        }
+
         NativeQuery query = NativeQuery.builder()
-                .withQuery(q -> q.bool(b -> {
-                    // 키워드 검색
-                    if (keyword != null && !keyword.isBlank()) {
-                        // 순서 그대로 or 단어 사이에 최대 한 단어 허용
-                        b.should(m -> m.matchPhrase(mm -> mm
-                                .field("productName")
-                                .query(keyword)
-                                .slop(1)
-                                .boost(2.0f)
-                        ));
-
-                        //단어 중 하나만 있어도 매칭
-                        b.should(m -> m.match(mm -> mm
-                                .field("productName")
-                                .query(keyword)
-                                .operator(Operator.Or)
-                                .boost(1.0f)
-                        ));
-
-                        b.should(m -> m.match(mm -> mm
-                                .field("description")
-                                .query(keyword)
-                                .operator(Operator.Or)
-                                .boost(1.0f)
-                        ));
-
-                        b.minimumShouldMatch("1");
-                    }
-
-                    // 카테고리 필터
-                    if (categories != null && !categories.isEmpty()) {
-                        b.filter(f -> f.terms(t -> t
-                                .field("categories")
-                                .terms(tv -> tv.value(
-                                        categories.stream()
-                                                .map(FieldValue::of)
-                                                .toList()
-                                ))
-                        ));
-                    }
-
-                    // 상태 필터
-                    if (status != null && !status.isBlank()) {
-                        b.filter(f -> f.term(t -> t.field("status").value(status)));
-                    }
-
-                    return b;
-                }))
+                .withQuery(q -> q.bool(boolQuery.build()))
                 .withSort(s -> s.score(sc -> sc.order(SortOrder.Desc))) // 점수순
                 .withSort(s -> s.field(f -> f.field("auctionStartAt").order(SortOrder.Desc))) // 최신순
                 .withPageable(pageable)
