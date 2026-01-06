2332f7f3e3e629d19220bba0b2593a9a6278e2a5
Geun-Hwan <rmsghks1026@gmail.com>
2026-01-06 13:44:14 +0900

    chore: order restful
    summary:
    
    footer:

--- settlement/src/main/java/com/dev_high/settlement/presentation/order/OrderController.java
+++ settlement/src/main/java/com/dev_high/settlement/presentation/order/OrderController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1/order")
+@RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
 public class OrderController {
 
