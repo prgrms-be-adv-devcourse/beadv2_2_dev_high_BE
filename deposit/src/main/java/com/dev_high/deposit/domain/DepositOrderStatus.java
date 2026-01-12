62867987e6dd08aa19c18a9ab4cf07c133459f8d
jtlee <jongtan.lee@gmail.com>
2026-01-12 11:37:51 +0900

    refactor: enum 추가

--- deposit/src/main/java/com/dev_high/deposit/domain/DepositOrderStatus.java
+++ deposit/src/main/java/com/dev_high/deposit/domain/DepositOrderStatus.java
@@ -3,12 +3,14 @@ package com.dev_high.deposit.domain;
 /*
  * 예치금 주문 상태 정의
  * PENDING : 대기 (결제 요청 직후)
- * COMPLETED : 완료 (충전/환불 성공)
- * FAILED : 실패 (결제 실패, 취소)
+ * PAYMENT_CONFIRMED : 결제 완료 (PG사 승인 성공)
+ * COMPLETED : 충전 완료 (충전/환불 성공)
+ * FAILED : 실패 (결제 실패)
  * CANCELLED : 취소 (사용자 요청에 의한 취소)
  * */
 public enum DepositOrderStatus {
     PENDING,
+    PAYMENT_CONFIRMED,
     COMPLETED,
     FAILED,
     CANCELLED
