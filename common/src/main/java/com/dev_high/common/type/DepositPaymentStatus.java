5b7f5d6336ddca9ed68d7188a322d1fb1ac8743a
jtlee <jongtan.lee@gmail.com>
2026-01-16 16:34:01 +0900

    refactor: DepositPaymentStatus 코드 정리

--- common/src/main/java/com/dev_high/common/type/DepositPaymentStatus.java
+++ common/src/main/java/com/dev_high/common/type/DepositPaymentStatus.java
@@ -5,13 +5,11 @@ package com.dev_high.common.type;
  * READY : 결제 준비
  * CONFIRMED : 결제 승인
  * CANCELED : 결제 취소
- * FAILED : 결제 실패
  * CONFIRMED_FAILED : 결제 승인 실패
  * */
 public enum DepositPaymentStatus {
     READY,
     CONFIRMED,
     CANCELED,
-    FAILED,
     CONFIRMED_FAILED
 }
