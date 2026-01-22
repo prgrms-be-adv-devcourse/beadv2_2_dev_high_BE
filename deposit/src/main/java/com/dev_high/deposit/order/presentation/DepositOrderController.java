f64176e15f47b850eb953167c71144e824091ce7
jtlee <jongtan.lee@gmail.com>
2026-01-22 11:57:16 +0900

    refactor: 결제 주문 쪽 컨트롤러 엔드포인트 부터 리팩터링

--- deposit/src/main/java/com/dev_high/deposit/order/presentation/DepositOrderController.java
+++ deposit/src/main/java/com/dev_high/deposit/order/presentation/DepositOrderController.java
@@ -1,7 +1,6 @@
 package com.dev_high.deposit.order.presentation;
 
 import com.dev_high.common.dto.ApiResponseDto;
-import com.dev_high.common.type.DepositOrderStatus;
 import com.dev_high.deposit.order.application.DepositOrderService;
 import com.dev_high.deposit.order.application.dto.DepositOrderDto;
 import com.dev_high.deposit.order.presentation.dto.DepositOrderRequest;
@@ -10,11 +9,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/payments")
 @RequiredArgsConstructor
@@ -26,9 +27,11 @@ public class DepositOrderController {
     @PostMapping("/orders/order-payment")
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponseDto<DepositOrderResponse.Detail> createPaymentOrder(@RequestBody @Valid DepositOrderRequest.CreatePayment request) {
+        log.info("[PaymentOrder API] PATH : /orders/order-payment request received. amount={}, deposit={}", request.amount(), request.deposit());
         DepositOrderDto.CreatePaymentCommand command = request.toCommand(request.amount(), request.deposit());
         DepositOrderDto.Info info = depositOrderService.createPaymentOrder(command);
         DepositOrderResponse.Detail response = DepositOrderResponse.Detail.from(info);
+        log.info("[PaymentOrder API] PATH : /orders/order-payment success. orderId={}, amount={}, deposit={}, paidAmount={}, status={}", response.id(), response.amount(), response.deposit(), response.paidAmount(), response.status());
         return ApiResponseDto.success(response);
     }
 
@@ -36,17 +39,21 @@ public class DepositOrderController {
     @PostMapping("/orders/deposit-charge")
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponseDto<DepositOrderResponse.Detail> createDepositPaymentOrder(@RequestBody @Valid DepositOrderRequest.CreateDepositPayment request) {
+        log.info("[PaymentOrder API] PATH : /orders/deposit-charge request received. amount={}", request.amount());
         DepositOrderDto.CreateDepositPaymentCommand command = request.toCommand(request.amount());
         DepositOrderDto.Info info = depositOrderService.createDepositPaymentOrder(command);
         DepositOrderResponse.Detail response = DepositOrderResponse.Detail.from(info);
+        log.info("[PaymentOrder API] PATH : /orders/deposit-charge success. orderId={}, amount={}, paidAmount={}, status={}", response.id(), response.amount(), response.paidAmount(), response.status());
         return ApiResponseDto.success(response);
     }
 
     @Operation(summary = "로그인한 사용자 ID의 예치금 주문 조회", description = "사용자 ID로 예치금 주문 내역 을 조회")
     @GetMapping("/orders/me")
     public ApiResponseDto<Page<DepositOrderResponse.Detail>> findByUserId(Pageable pageable) {
+        log.info("[PaymentOrder API] PATH : /orders/me request received. page={}, size={}, sort={}", pageable.getPageNumber(), pageable.getPageSize(), pageable.getSort());
         Page<DepositOrderDto.Info> infos = depositOrderService.findByUserId(pageable);
         Page<DepositOrderResponse.Detail> response = infos.map(DepositOrderResponse.Detail::from);
+        log.info("[PaymentOrder API] PATH : /orders/me success. totalElements={}, totalPages={}", response.getTotalElements(), response.getTotalPages());
         return ApiResponseDto.success(response);
     }
 
@@ -54,12 +61,11 @@ public class DepositOrderController {
     @PostMapping("/orders/pay-by-deposit")
     public ApiResponseDto<?> payOrderByDeposit(@RequestBody @Valid DepositOrderRequest.OrderPayWithDeposit request) {
         DepositOrderDto.OrderPayWithDepositCommand command = request.toCommand(request.id());
-        DepositOrderDto.Info info = depositOrderService.payOrderByDeposit(command);
-        if (info.status().equals(DepositOrderStatus.DEPOSIT_APPLIED_ERROR)) {
-            return ApiResponseDto.fail("예치금 사용에 실패하였습니다", info.status().name());
-        } else {
-            DepositOrderResponse.Detail response = DepositOrderResponse.Detail.from(info);
-            return ApiResponseDto.success(response);
+        try {
+            DepositOrderDto.Info info = depositOrderService.payOrderByDeposit(command);
+            return ApiResponseDto.success(DepositOrderResponse.Detail.from(info));
+        } catch (Exception e) {
+            return ApiResponseDto.fail("예치금 사용에 실패하였습니다", e.getMessage());
         }
     }
 }
